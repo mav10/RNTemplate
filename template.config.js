eb40f8e923f5a08f6dd715d220260cdd14508f10
maxim vasin <maxim.vasin@mcc-tomsk.de>
2022-09-13 14:15:10 +0700

    upd

--- template.config.js
+++ template.config.js
@@ -7,7 +7,7 @@ module.exports = {
 
   // Placeholder title that will be replaced in values.xml and Info.plist with title provided by the user.
   // We default this value to 'Hello App Display Name', which is default placeholder in react-native template.
-  titlePlaceholder: 'RN Template App',
+  titlePlaceholder: 'rntemplateapp',
 
   // Path to script, which will be executed after initialization process, but before installing all the dependencies specified in the template. This script runs as a shell script but you can change that (e.g. to Node) by using a shebang (see example custom template).
   postInitScript: './postinit-script.js',
